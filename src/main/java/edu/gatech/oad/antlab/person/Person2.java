7eb44dca41d8f0a4d2b8cdff54ec4b6dee6f3ed3
Kenneth Adams <khetnen@gmail.com>
2015-01-23 17:20:54 -0500

    implemented Person2.java

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,12 +1,17 @@
 package edu.gatech.oad.antlab.person;
 
+import java.lang.String;
+import java.lang.StringBuilder;
+import java.util.List;
+import java.util.Random;
+
 /**
  *  A simple class for person 2
  *  returns their name and a
  *  modified string 
  *
- * @author Bob
- * @version 1.1
+ * @author Kenneth Adams
+ * @version 1.2
  */
 public class Person2 {
     /** Holds the persons real name */
@@ -30,8 +35,13 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+		Random random = new Random();
+		StringBuilder output = new StringBuilder(input.length());
+		List<char> charList = new ArrayList<char>(input.toCharArray());
+		while( chars.length > 0 ) {
+			output.append( charList.remove( random.nextInt(chars.length) ) );
+		}
+	  	return output.toString();
 	}
 	/**
 	 * Return a string rep of this object
