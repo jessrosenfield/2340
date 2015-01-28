01455e9dbe952cd95cf2e5471017a7ebfaed2e6d
Manas George <manas.george@gatech.edu>
2015-01-28 10:35:03 -0500

    Changed email strings to username, fixed bug where backing out from application screen goes back to login.

--- app/src/main/java/com/gatech/objectsanddesign/shoppingwithfriends/LoginActivity.java
+++ app/src/main/java/com/gatech/objectsanddesign/shoppingwithfriends/LoginActivity.java
@@ -6,6 +6,7 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
@@ -287,6 +288,8 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
             showProgress(false);
 
             if (success) {
+                Intent i = new Intent(LoginActivity.this, ApplicationScreen.class);
+                startActivity(i);
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
